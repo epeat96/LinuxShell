6f1a01172fba7cffff0950a94bbdd00572486eb1
Cristobal961 <cristobalbarreto9@gmail.com>
2019-01-29 20:13:34 -0300

    Se cambiaron los controles de copiar y mover para que sean manejando excepciones y ademas ambos ahora funcionan para directorios de forma recursiva

--- shell/commandsImpl/service.py
+++ shell/commandsImpl/service.py
@@ -1,18 +1,61 @@
 import os
+import shutil
+def lenctrl(tokens):
+    if len(tokens)>2: # Si se introdujeron mas de 2 parametros
+        print("Se introdujeron mas parametros de los aceptados")
+        return False
+    elif len(tokens)<2:
+        print("Muy pocos parametros ingresados")
+        return False
+    return True
 
 def copiar(a,b):
-    if os.path.isfile(a) :
-        a=open(a,"r")
-    else :
-        print("\""+a+"\""+" no existe o no es un archivo")
-
-    if os.path.isfile(b) :
-        print("\""+b+"\""+" ya existe o no es un archivo")
-    else :
-        if os.path.isdir(b) :
-            print(b+" ya existe o no es un archivo")
-        else :
-            b=open(b,"w")
-            b.write(a.read())
-            a.close()
-            b.close()
+    #Si a existe y si b no existe
+    if os.path.exists(a) and not os.path.exists(b):
+        #copiar directorio recursivamente o el archivo
+        if os.path.isfile(a): # Si a es un archivo
+            #se intenta abrir a en modo lectura
+            try:
+                a=open(a,"r")
+            #Mensaje de error en caso de que a no sea un archivo, no exista o la ruta sea invalida
+            except IOError:
+                print("El archivo o directorio: "+"\""+a+"\""+" no existe")
+                return 1
+            if os.path.isfile(b):
+                #Mensaje de error en caso de que b no sea un archivo, no exista o la ruta sea invalida
+                print("El archivo o directorio: "+"\""+b+"\""+" ya existe o la ruta no es valida")
+                return 1
+            else :
+                #se intenta abrir b en modo escritura
+                try:
+                    b=open(b,"w")
+                except IOError:
+                    #Mensaje de error en caso de que b no sea un archivo, no exista o la ruta sea invalida
+                    print("El archivo o directorio: "+"\""+b+"\""+" ya existe o la ruta no es valida")
+                    return 1
+                b.write(a.read())
+                return 0
+        #si a es un directorio
+        elif os.path.isdir(a):
+            #se intenta copiar el arbol de directorios
+            try:
+                shutil.copytree(a, b)
+            #Mensaje de error en caso de que no se pueda
+            except IOError:
+                print("El archivo o directorio: "+"\""+b+"\""+" ya existe o la ruta no es valida")
+                return 1
+        #mensaje de error si a no es un directorio
+        else:
+            print("El archivo o directorio: "+"\""+a+"\""+" no existe")
+            return 1
+        return 0
+    else:
+        #Mensajes de error
+        #Caso 1 El parametro a no existe (archivo o directorio a copiar)
+        if not os.path.exists(a):
+            print("El archivo o directorio: "+"\""+a+"\""+" no existe")
+            return 1
+        #Caso 2 El parametro b ya existe (ruta donde se debe copiar el archivo o directorio)
+        elif os.path.exists(b):
+            print("El archivo o directorio: "+"\""+b+"\""+" ya existe")
+            return 1
--- shell/shell.py
+++ shell/shell.py
@@ -1,48 +1,44 @@
 from cmd import Cmd
 import os
 import optparse
+import shutil
 from helpImpl import myHelp
 from commandsImpl import service
-
 class MyPrompt(Cmd):
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     def help_copiar(self):
         myHelp.copiar()
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     '''
     Funcion copiar
     Recibe como parametros las rutas de un archivo a copiar y la ruta donde este sera copiado
     '''
     def do_copiar(self, inp):
         tokens = inp.split()
-        if len(tokens)>2:
-            print("No se reconocen los siguientes parametros:")
-            for x in range(2, len(tokens)):
-                print(tokens[x])
-        else:
+        #si la cantidad de parametros es correcta
+        if service.lenctrl(tokens):
+            #se intenta copiar
             service.copiar(tokens[0],tokens[1])
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     '''
     Funcion mover
-    Recibe como paramtetros las rutas de un archivo a mover y la ruta donde este sera movido, tambien se puede usar
+    Recibe como parametros las rutas de un archivo a mover y la ruta donde este sera movido, tambien se puede usar
     para renombrar archivos
     '''
     def do_mover(self, inp):
         tokens = inp.split()
-        if len(tokens)>2:
-            print("No se reconocen los siguientes parametros:")
-            for x in range(2, len(tokens)):
-                print(tokens[x])
-        else:
-            service.copiar(tokens[0],tokens[1])
-            os.remove(tokens[0])
-#+++++++++++++++++++++++++++++++++++++++
-    def do_hola(self, inp):
-        print("holamundo")
-#++++++++++++++++++++++++++++++++++++++
-    def do_exit(self, inp):
-        print("Bye")
-        return True
-#+++++++++++++++++++++++++++++++++++++++++++
-    def do_add(self, inp):
-        print("Adding '{}'".format(inp))
+        #si la cantidad de parametros es correcta
+        if service.lenctrl(tokens):
+            #si se pudo copiar
+            if service.copiar(tokens[0],tokens[1]) == 0:
+                shutil.rmtree(tokens[0])
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    '''
+    Funcion renombrar
+    Recibe com parametros la ruta de un archivo y el nombre con el cual este se quiere do_renombrar
+    '''
+    def do_renombrar(self, inp):
+        tokens = inp.split()
 
 MyPrompt().cmdloop()
 print("after")
