8b77c2c125ce48b459cd1ccf2dedda8f10300857
Cristobal961 <cristobalbarreto9@gmail.com>
2019-01-28 21:36:49 -0300

    cambio de /shell/commandsImpl/copiar.py a /shell/commandsImpl/service.py

--- shell/shell.py
+++ shell/shell.py
@@ -2,7 +2,7 @@ from cmd import Cmd
 import os
 import optparse
 from helpImpl import myHelp
-from commandsImpl import copiar
+from commandsImpl import service
 class MyPrompt(Cmd):
     def help_copiar(self):
         myHelp.copiar()
@@ -29,13 +29,16 @@ class MyPrompt(Cmd):
             for x in range(2, len(tokens)):
                 print(tokens[x])
         else:
-            copiar.copiar(tokens[0],tokens[1])
+            serice.copiar(tokens[0],tokens[1])
             os.remove(tokens[0])
+#+++++++++++++++++++++++++++++++++++++++
     def do_hola(self, inp):
         print("holamundo")
+#++++++++++++++++++++++++++++++++++++++
     def do_exit(self, inp):
         print("Bye")
         return True
+#+++++++++++++++++++++++++++++++++++++++++++
     def do_add(self, inp):
         print("Adding '{}'".format(inp))
 
