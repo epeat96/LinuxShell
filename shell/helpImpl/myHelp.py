923d151072fc266a0165f294d739691556841135
Cristobal961 <cristobalbarreto9@gmail.com>
2019-01-29 21:20:59 -0300

    Cambio de nombre a funciones para no necesitar controlar que no se llamen a los programas default de linux

--- shell/helpImpl/myHelp.py
+++ shell/helpImpl/myHelp.py
@@ -6,5 +6,4 @@ def copiar():
 def mover():
     print("Uso:")
     print("mover [ruta archivo a copiar] [ruta donde se va a mover]")
-    print("Para renombrar un archivo se puede utilizar la funcion mover")
-    print("colocando la misma ruta pero con nombres de archivo diferentes")
+    print("OBS: \nPara renombrar un archivo se puede utilizar la funcion mover, colocando la misma ruta pero con nombres de archivo diferentes")
--- shell/shell.py
+++ shell/shell.py
@@ -5,15 +5,23 @@ import shutil
 from helpImpl import myHelp
 from commandsImpl import service
 class MyPrompt(Cmd):
+    ruler='+'
+    doc_header="Los siguientes comandos estan documentados, para verlos ejecute: help: <comando>"
+    misc_header="Documentacion de ayuda miscelanea"
+    undoc_header="Los siguientes comandos no estan documentados:"
+    prompt="#"
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    def help_copiar(self):
+    def help_cp(self):
         myHelp.copiar()
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    def help_mover(self):
+        myHelp.mover()
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     '''
-    Funcion copiar
+    Funcion cp
     Recibe como parametros las rutas de un archivo a copiar y la ruta donde este sera copiado
     '''
-    def do_copiar(self, inp):
+    def do_cp(self, inp):
         tokens = inp.split()
         #si la cantidad de parametros es correcta
         if service.lenctrl(tokens,2):
@@ -21,11 +29,11 @@ class MyPrompt(Cmd):
             service.copiar(tokens[0],tokens[1])
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     '''
-    Funcion mover
+    Funcion mv
     Recibe como parametros las rutas de un archivo a mover y la ruta donde este sera movido, tambien se puede usar
     para renombrar archivos
     '''
-    def do_mover(self, inp):
+    def do_mv(self, inp):
         tokens = inp.split()
         #si la cantidad de parametros es correcta
         if service.lenctrl(tokens,2):
@@ -37,7 +45,7 @@ class MyPrompt(Cmd):
     Funcion renombrar
     Recibe com parametros la ruta de un archivo y el nombre con el cual este se quiere do_renombrar
     '''
-    def do_renombrar(self, inp):
+    def do_rn(self, inp):
         tokens = inp.split()
 
 MyPrompt().cmdloop()
